d0d2a32da719414fdc1d105994761dfcd1b0130a
Oleksandr Kharchenko <dnc.oleksandr.kharchenko@gmail.com>
2023-06-21 20:45:09 +0300

    update schemas

--- internal/infrastructure/repository/mongo/schemas.go
+++ internal/infrastructure/repository/mongo/schemas.go
@@ -6,10 +6,28 @@ import (
 )
 
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Name      string             `bson:"name,omitempty"`
-	Email     string             `bson:"email,omitempty"`
-	Password  string             `bson:"password,omitempty"`
-	CreatedAt time.Time          `bson:"created_at,omitempty"`
-	UpdatedAt time.Time          `bson:"updated_at,omitempty"`
+	ID        primitive.ObjectID   `bson:"_id,omitempty"`
+	Boards    []primitive.ObjectID `bson:"boards,omitempty"`
+	CreatedAt time.Time            `bson:"created_at,omitempty"`
+	UpdatedAt time.Time            `bson:"updated_at,omitempty"`
+	Name      string               `bson:"name,omitempty"`
+	Email     string               `bson:"email,omitempty"`
+	Password  string               `bson:"password,omitempty"`
+}
+
+type Note struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Board       primitive.ObjectID `bson:"board,omitempty"`
+	CreatedAt   time.Time          `bson:"created_at,omitempty"`
+	UpdatedAt   time.Time          `bson:"updated_at,omitempty"`
+	Description string             `bson:"description,omitempty"`
+}
+
+type Board struct {
+	ID          primitive.ObjectID   `bson:"_id,omitempty"`
+	Members     []primitive.ObjectID `bson:"members,omitempty"`
+	CreatedAt   time.Time            `bson:"created_at,omitempty"`
+	UpdatedAt   time.Time            `bson:"updated_at,omitempty"`
+	Title       string               `bson:"title,omitempty"`
+	Description string               `bson:"description,omitempty"`
 }
